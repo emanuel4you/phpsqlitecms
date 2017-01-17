8ac0ed55e26010775b8b982d6d7b4218b95aabf7
Emanuel Strobel <emanuel@ihad.tv>
2017-01-17 23:37:56 +0100

    Fix picture path TinyMCE & add some features

--- static/js/wysiwyg_init.js
+++ static/js/wysiwyg_init.js
@@ -1,14 +1,15 @@
 tinymce.init({
    selector: "#content",
+   themes: "modern",
    menubar:false,
    statusbar: false,
    entity_encoding : "raw",
    plugins: [
-        "advlist autolink lists link image anchor code"
+        "advlist autolink lists link media image imagetools anchor code hr paste autoresize charmap textcolor colorpicker preview searchreplace visualblocks fullscreen"
     ],
    target_list:false,
     
-   toolbar: "undo redo | styleselect | bold italic | bullist numlist | link unlink | image | code",
-   content_css : "../static/css/wysiwyg.css"
-
+   toolbar: "undo redo | searchreplace | styleselect | bold italic | forecolor backcolor | removeformat | hr | bullist numlist | link unlink | image media | charmap | code | template | visualblocks | fullscreen preview",
+   content_css : "../static/css/wysiwyg.css",
+   document_base_url: window.location.origin
 });
